5f0e0f89a60fd98ee108b312b241d43a67d9a6ff
slhun22 <yangsh5106@gmail.com>
2023-07-14 16:21:04 +0900

    중간발표 빌드 파일 커밋 (잔버그 수정)

---
+++ Scripts/UI/LoadingText.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadingText : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI loadingText;
+
+    List<string> tipsList = new List<string>();
+}
