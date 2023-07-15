9c7b93305e60e1c4e4cb0d9a97da38eb0a24ab53
escatrgot <escatrgot@gmail.com>
2023-07-15 09:46:29 +0900

    CircleCast

---
+++ Scripts/Objects/Marker/CircleCaster.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+public class CircleCaster : EventMarker
+{
+    //public LayerMask castTargets;
+    //protected RaycastHit2D[] hits;
+    [SerializeField] UnityEvent OnTriggerEvents;
+    
+    protected override void FixedUpdate() {
+        hits = GetCastedTarget();
+        if(hits.Count() == 0) {isActivated = false;}
+        else if(hits.Count() != 0 && !isActivated)
+        {
+            isActivated = true;
+            OnTriggerEvents.Invoke();
+            Destroy(gameObject);
+        }
+    }
+    protected override RaycastHit2D[] GetCastedTarget()
+    {
+        return hits = Physics2D.CircleCastAll(transform.position, 0.75f, Vector2.zero, castTargets);
+    }
+}
