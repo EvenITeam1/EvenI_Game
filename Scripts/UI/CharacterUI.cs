d4c6bb49b50746b5a470011995964b3e3055f962
slhun22 <yangsh5106@gmail.com>
2023-07-30 03:42:54 +0900

    캐릭터선택창 인게임 연동부분 빼고 완성

--- Scripts/UI/CharacterUI.cs
+++ Scripts/UI/CharacterUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class CharacterUI : MonoBehaviour
@@ -9,7 +10,15 @@ public class CharacterUI : MonoBehaviour
     public PlayerData playerData;
     public bool isUnlocked;
     SpriteRenderer spriteRenderer;
-    [SerializeField] TextMeshProUGUI tmpro;
+    [SerializeField] Button selectButton;
+    [SerializeField] TextMeshProUGUI circleNameText;
+
+    [SerializeField] TextMeshProUGUI infoNameText;
+    [SerializeField] TextMeshProUGUI infoeScriptText;
+    [SerializeField] TextMeshProUGUI infoHpText;
+    [SerializeField] TextMeshProUGUI infoHpRecoverText;
+    [SerializeField] TextMeshProUGUI infoBasicAttText;
+    [SerializeField] TextMeshProUGUI infoJumpAttText;
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -29,7 +38,34 @@ public class CharacterUI : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("asdf");
-        tmpro.text = "<" + playerData.Character_engname + ">";
+        circleNameText.text = "<" + playerData.Character_engname + ">";
+        refreshMoreInfoAndSelectButton();
+    }
+
+    void refreshMoreInfoAndSelectButton()
+    {
+        if (!isUnlocked)
+        {
+            selectButton.interactable = false;
+            infoNameText.text = "???";
+            infoeScriptText.text = "보유하지 않은 캐릭터입니다.";
+            infoHpText.text = "상점에서";
+            infoHpRecoverText.text = "캐릭터를";
+            infoBasicAttText.text = "만나보세요";
+            infoJumpAttText.text = "!!!!";         
+        }
+
+        else
+        {
+            selectButton.interactable = true;
+            infoNameText.text = playerData.Character_korname;
+            infoeScriptText.text = playerData.Character_script;
+            infoHpText.text = $"체력 : {playerData.Character_hp}";
+            var basicBulletData = GameManager.Instance.BulletDataTableDesign.GetBulletDataByINDEX(playerData.Character_bullet_index_1);
+            var jumpBulletData = GameManager.Instance.BulletDataTableDesign.GetBulletDataByINDEX(playerData.Character_bullet_index_2);
+            infoHpRecoverText.text = $"회복속도 : {playerData.Character_per_hp_heal}/s";
+            infoBasicAttText.text = $"일반공격력 : {basicBulletData.Bullet_min_dmg} ~ {basicBulletData.Bullet_max_dmg}";
+            infoJumpAttText.text = $"점프공격력 : {jumpBulletData.Bullet_min_dmg} ~ {jumpBulletData.Bullet_max_dmg}";        
+        }    
     }
 }
